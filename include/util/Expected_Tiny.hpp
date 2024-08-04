1e3f5d6174ddfafc02b1bc0b75ff2d734aeddece
yao <1746254723@qq.com>
2024-08-05 03:56:36 +0800

    update

--- include/util/Expected_Tiny.hpp
+++ include/util/Expected_Tiny.hpp
@@ -9,7 +9,7 @@
 namespace hy {
 /*details...*/
 namespace details {
-  enum class ExpectedStorageType {
+enum class ExpectedStorageType {
   ePODStruct,
   ePODUnion,
   eUnion,
@@ -106,6 +106,68 @@ class ExpectedStorage {
 
   constexpr E&& error() && noexcept { return std::move(unex_); }
 
+  template <class U>
+  constexpr V value_or(U&& default_value) const& {
+    static_assert(std::conjunction_v<std::is_copy_constructible<V>,
+                                     std::is_convertible<U, V>>,
+                  "");
+    return has_value() ? **this
+                       : static_cast<V>(std::forward<U>(default_value));
+  }
+
+  template <class U>
+  constexpr V value_or(U&& default_value) && {
+    static_assert(std::conjunction_v<std::is_move_constructible<V>,
+                                     std::is_convertible<U, V>>,
+                  "");
+    return has_value() ? std::move(**this)
+                       : static_cast<V>(std::forward<U>(default_value));
+  }
+
+  template <class G = E>
+  constexpr E error_or(G&& default_value) const& {
+    static_assert(std::conjunction_v<std::is_copy_constructible<E>,
+                                     std::is_convertible<G, E>>,
+                  "");
+    return has_value() ? std::forward<G>(default_value) : error();
+  }
+
+  template <class G = E>
+  constexpr E error_or(G&& default_value) && {
+    static_assert(std::conjunction_v<std::is_move_constructible<E>,
+                                     std::is_convertible<G, E>>,
+                  "");
+    return has_value() ? std::forward<G>(default_value) : std::move(error());
+  }
+
+  /*sengial operator*/
+
+  /*Modify*/
+  template <class... Args, typename = std::enable_if_t<
+                               std::is_nothrow_constructible_v<V, Args...>>>
+  constexpr V& emplace(Args&&... args) noexcept {
+    if (has_value()) {
+      ~val_;
+    } else {
+      ~unex_;
+    }
+    return *std::construct_at(std::addressof(val_),
+                              std::forward<Args>(args)...);
+  }
+
+  template <class U, class... Args,
+            typename = std::enable_if_t<std::is_nothrow_constructible_v<
+                V, std::initializer_list<U>&, Args...>>>
+  constexpr V& emplace(std::initializer_list<U> il, Args&&... args) noexcept {
+    if (has_value()) {
+      ~val_;
+    } else {
+      ~unex_;
+    }
+    return *std::construct_at(std::addressof(val_), il,
+                              std::forward<Args>(args)...);
+  }
+
  private:
   bool has_val_;
   union {
@@ -116,19 +178,9 @@ class ExpectedStorage {
 
 }  // namespace details
 
-
-
-
-
-
-
-
-template<typename V, typename E>
+template <typename V, typename E>
 class expected final : public details::ExpectedStorage<V, E> {
-  public:
-
-
-
+ public:
 };
 
 }  //namespace hy
