9d829283a3f84bc9d4b9a09760b5d31836d3e2d1
you_wsl_company <you@1746254723@qq.com>
2024-10-21 19:36:53 +0800

    update

--- include/net/Basic_endpoint.h
+++ include/net/Basic_endpoint.h
@@ -1,40 +1,35 @@
 #ifndef HY_BASIC_ENDPOINT_H_
 #define HY_BASIC_ENDPOINT_H_
-#include <cstdint>
 #include <cstddef>
+#include <cstdint>
 #include "Net_type.hpp"
 
+namespace hy {
+namespace net {
 
-namespace hy{
-    namespace net{
-        
-        template <typename InternetProtocol>
-        class basic_endpoint {
-            public:
-            using protocol_type = InternetProtocol;
-            using data_type = socket_addr_type;
+template <typename InternetProtocol>
+class basic_endpoint {
+ public:
+  using protocol_type = InternetProtocol;
+  using data_type = sockaddr_type;
 
-            basic_endpoint() noexcept;
+  constexpr basic_endpoint() noexcept = default;
+  constexpr basic_endpoint(const basic_endpoint&) noexcept = default;
+  constexpr basic_endpoint(basic_endpoint&&) noexcept = default;
+  constexpr basic_endpoint& operator=(const basic_endpoint&) = default;
+  constexpr basic_endpoint& operator=(basic_endpoint&&) = default;
+  ~constexpr basic_endpoint() = default;
 
+  basic_endpoint(const InternetProtocol& internet_protocol,
+                 port_type port) noexcept
+      : impl_(internet_protocol.family(), port) {}
 
+    
 
-
-            private:
-            ::hy::net::detail::Endpoint impl_;
-
-        };
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
+ private:
+  ::hy::net::detail::Endpoint impl_;
+};
+}  // namespace net
+}  // namespace hy
 
 #endif  //HY_BASIC_ENDPOINT_H_
